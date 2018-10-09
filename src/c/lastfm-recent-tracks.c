3c2f5f3254fa68bbb895c83c2846b3857fa07141
J. Tran <jyntran@gmail.com>
2018-10-09 19:54:29 -0400

    refactor

--- src/c/lastfm-recent-tracks.c
+++ src/c/lastfm-recent-tracks.c
@@ -5,9 +5,9 @@ static Window *s_window;
 static Layer *track_layer, *header_layer;
 static TextLayer *s_track_layer, *s_artist_layer, *s_timestamp_layer, *s_total_layer;
 
-static char track_data[4096];
-static char artist_data[4096];
-static char timestamp_data[4096];
+static char track_data[DATA_SIZE];
+static char artist_data[DATA_SIZE];
+static char timestamp_data[TIMESTAMP_DATA_SIZE];
 
 static int current = 0;
 
@@ -95,7 +95,7 @@ static void prv_click_config_provider(void *context) {
 
 static void track_update_proc(Layer *layer, GContext *ctx) {
   if (tracks[current]) {
-    static char track_buffer[2048];
+    static char track_buffer[BUFFER_SIZE];
     static unsigned int track_length;
     if (!isCurrentLast()) {
       track_length = strlen(tracks[current]) - strlen(tracks[current+1]);
@@ -108,7 +108,7 @@ static void track_update_proc(Layer *layer, GContext *ctx) {
   }
 
   if (artists[current]) {
-    static char artist_buffer[2048];
+    static char artist_buffer[BUFFER_SIZE];
     static unsigned int artist_length;
     if (!isCurrentLast()) {
       artist_length = strlen(artists[current]) - strlen(artists[current+1]);
@@ -121,7 +121,7 @@ static void track_update_proc(Layer *layer, GContext *ctx) {
   }
 
   if (timestamps[current]) {
-    static char timestamp_buffer[2048];
+    static char timestamp_buffer[TIMESTAMP_BUFFER_SIZE];
     static unsigned int timestamp_length;
     if (!isCurrentLast()) {
       timestamp_length = strlen(timestamps[current]) - strlen(timestamps[current+1]);
@@ -135,7 +135,7 @@ static void track_update_proc(Layer *layer, GContext *ctx) {
 }
 
 static void header_update_proc(Layer *layer, GContext *ctx) {
-  static char total_buffer[8];
+  static char total_buffer[HEADER_BUFFER_SIZE];
   snprintf(total_buffer, sizeof(total_buffer), "%d/%d", current+1, LIMIT);
   APP_LOG(APP_LOG_LEVEL_DEBUG, total_buffer);
   text_layer_set_text(s_total_layer, total_buffer);
@@ -148,26 +148,30 @@ static void prv_window_load(Window *window) {
   track_layer = layer_create(bounds);
   header_layer = layer_create(bounds);
 
-  s_track_layer = text_layer_create(GRect(0, 40, bounds.size.w, 20));
+  s_track_layer = text_layer_create(GRect(TRACK_POS_X, TRACK_POS_Y, bounds.size.w, TRACK_SIZE_H));
   text_layer_set_text_alignment(s_track_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_track_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(track_layer, text_layer_get_layer(s_track_layer));
 
-  s_artist_layer = text_layer_create(GRect(0, 60, bounds.size.w, 20));
+  s_artist_layer = text_layer_create(GRect(ARTIST_POS_X, ARTIST_POS_Y, bounds.size.w, ARTIST_SIZE_H));
   text_layer_set_text_alignment(s_artist_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_artist_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   layer_add_child(track_layer, text_layer_get_layer(s_artist_layer));
 
-  s_timestamp_layer = text_layer_create(GRect(0, 80, bounds.size.w, 20));
+  s_timestamp_layer = text_layer_create(GRect(TIMESTAMP_POS_X, TIMESTAMP_POS_Y, bounds.size.w, TIMESTAMP_SIZE_H));
   text_layer_set_text_alignment(s_timestamp_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_timestamp_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   layer_add_child(track_layer, text_layer_get_layer(s_timestamp_layer));
 
-  s_total_layer = text_layer_create(GRect(0, 20, bounds.size.w, 20));
+  s_total_layer = text_layer_create(GRect(TOTAL_POS_X, TOTAL_POS_Y, bounds.size.w, TOTAL_SIZE_H));
   text_layer_set_text_alignment(s_total_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_total_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   layer_add_child(header_layer, text_layer_get_layer(s_total_layer));
 
   layer_add_child(window_layer, header_layer);
   layer_add_child(window_layer, track_layer);
 
-  text_layer_set_text(s_track_layer, "LOADING");
+  text_layer_set_text(s_track_layer, "Loading");
 
   layer_set_update_proc(track_layer, track_update_proc);
   layer_set_update_proc(header_layer, header_update_proc);
