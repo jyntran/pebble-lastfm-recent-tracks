e996a13ff9e3c5de1ad57546d9ac930777a7feb3
J. Tran <jyntran@gmail.com>
2018-10-13 18:26:18 -0400

    set up Clay

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,4 +1,11 @@
+var Clay = require('pebble-clay');
+var clayConfig = require('./config.json');
+var clay = new Clay(clayConfig, null, {autoHandleEvents: false});
+var messageKeys = require('message_keys');
+
 var env = require('./env.json');
+
+var lastfmUsername = '';
 
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
@@ -15,10 +22,10 @@ function hasSpecialCharacters(str) {
 }
 
 function getLastfmTracks() {
-  const username = env.lastfm.username;
-  const apiKey = env.lastfm.apiKey;
-  const limit = 3;
-  const url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks'
+  var username = lastfmUsername !== '' ? lastfmUsername : env.lastfm.username;
+  var apiKey = env.lastfm.apiKey;
+  var limit = 3;
+  var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks'
     + '&user=' + username + '&api_key=' + apiKey
     + '&limit=' + limit
     + '&format=json';
@@ -67,4 +74,21 @@ Pebble.addEventListener('ready',
 Pebble.addEventListener('appmessage', function(e) {
   var dict = e.payload;
   console.log('Got message: ' + JSON.stringify(dict));
+});
+
+Pebble.addEventListener('showConfiguration', function(e) {
+  Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var dict = clay.getSettings(e.response);
+
+  lastfmUsername = dict[messageKeys.LastfmUsername];
+  console.log('lastfmUsername: ', lastfmUsername);
+
+  Pebble.sendAppMessage(dict, function() {
+    console.log('Message sent successfully: ' + JSON.stringify(dict));
+  }, function(e) {
+    console.log('Message failed: ' + JSON.stringify(e));
+  });
 });
