78f63b9f7bbf2ae0585cb0508998b4680b6a0c2b
J. Tran <jyntran@gmail.com>
2018-10-17 22:32:58 -0400

    increase message size

--- src/c/settings.c
+++ src/c/settings.c
@@ -4,5 +4,5 @@ void prv_load_settings() {
   persist_read_data(SETTINGS_KEY, &settings, sizeof(settings));
 
   app_message_register_inbox_received(prv_inbox_received_handler);
-  app_message_open(256, 256);
+  app_message_open(512, 512);
 }
